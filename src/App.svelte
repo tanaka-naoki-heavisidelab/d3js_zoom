e662988686544482f5325e5d392116132828b77b
tanaka-naoki-heavisidelab <tanakanaoki@heavisidelab.jp>
2022-01-20 17:55:35 +0900

    1st commit: svelte,typescript, pug, sass,D3.js

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let name: string;
+</script>
+
+<template lang="pug">
+ main 
+   h1 Hello {name}!
+   p Visit the 
+   |
+   |
+   a(href="https://svelte.dev/tutorial") Svelte tutorial
+   |
+   | to learn how to build Svelte apps.
+</template>
+
+<style src="./App.scss">
+</style>
